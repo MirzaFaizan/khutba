816c7aab9cbe293c0dbf1587c0ea50b5daf3bf47
aniqmir <aniqmir@gmail.com>
2019-02-21 23:04:50 +0500

    youtube video integration

--- src/views/AdminScreen/AdminScreen.jsx
+++ src/views/AdminScreen/AdminScreen.jsx
@@ -1,18 +1,101 @@
 import React from 'react';
 import AppBar from '../AppBar/AppBar.jsx';
 import Grid from '@material-ui/core/Grid';
-
+import { Typography, TextField, Button } from '@material-ui/core';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+import Stop from '@material-ui/icons/Stop';
+import YouTube from 'react-youtube';
 
 class AdminScreen extends React.Component{
 
-    render() {
+    constructor(props){
+        super(props)
+        this.state={
+            videoId:'',
+            stream:false,
+        }
+    }
 
+    handleVideoid = e => {
+        this.setState({
+            videoId:e.target.value
+        })
+    }
+
+    startStream = () => {
+        this.setState({
+            stream:true
+        })
+    }
+
+    stopStream = () => {
+        this.setState({
+            stream:false,
+            videoId:''
+        })
+    }
+    render() {
+        const opts = {
+            height: '480',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+              autoplay: 1
+            }
+          };
         return (
             <div>
                 <AppBar name='Teacher' history={this.props.history}/>
-            <Grid container spacing={0}>
-                  
+            <div style={{padding:12}}>
+            <Grid container spacing={24} justify="center" alignContent="center" alignItems="center">
+                <Grid item xs={12}>
+                    <Typography align="center" variant="display3">
+                        You Can Start By Placing ID of video Here
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <div style={{paddingLeft:'10%',paddingRight:'10%'}}>
+                    <TextField 
+                      id="outlined-videid-input"
+                      label="Video ID"
+                      name="Video ID"
+                      value={this.state.videoId}
+                      margin="dense"
+                      variant="outlined"
+                      fullWidth={true}
+                    onChange={e=>this.handleVideoid(e)}
+                    />
+                    </div>
+                </Grid>
+                    {
+                      this.state.stream ? (
+                        <div>
+                        <Grid item xs={12}>
+                        <div style={{textAlign:'center'}}>
+                        <Button variant="contained" size="large" onClick={()=>this.stopStream()}>
+                        <Stop/>
+                        </Button>
+                        </div>
+                        </Grid>
+                        <div style={{paddingTop:'10%'}}>
+                            <YouTube
+                            videoId={this.state.videoId}
+                            opts={opts}
+                            />
+                         </div>
+                        </div>
+                      )
+                      : (
+                        <Grid item xs={12}>
+                        <div style={{textAlign:'center'}}>
+                        <Button variant="contained" size="large" onClick={()=>this.startStream()}>
+                        <PlayArrow/>
+                        </Button>
+                        </div>
+                        </Grid>
+                      )
+                    }
             </Grid>
+            </div>
             </div>
         )
     }
--- src/views/ClientScreen/ClientScreen.jsx
+++ src/views/ClientScreen/ClientScreen.jsx
@@ -1,21 +1,57 @@
 import React from 'react';
 import AppBar from '../AppBar/AppBar.jsx';
 import Grid from '@material-ui/core/Grid';
-import YoutubeLive from 'youtube-live-react';
+// import YoutubeLive from 'youtube-live-react';
+import { Typography } from '@material-ui/core';
+import YouTube from 'react-youtube';
 
 class ClientScreen extends React.Component{
 
+    constructor(props){
+        super(props)
+
+        this.state={
+            streamAvailable :true
+        }
+    }
     render() {
+        const opts = {
+            height: '480',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+              autoplay: 1
+            }
+          };
         return (
         <div>
         <AppBar name='User' history={this.props.history}/>
         <Grid container spacing={0}>
-        <YoutubeLive
-        iframeWidth={400}
-        iframeHeight={300}
-        maxResults={50}
-        youtubeChannelId='UCqLGw6DbXL8NaiM3p97TA6A '
-        googleApiKey='{YOUR_GOOGLE_API_KEY}'/>
+        {
+            this.state.streamAvailable ? (
+                <Grid item xs={12}>
+                    <Typography align="center" variant="display3">
+                        Stream Available
+                    </Typography>
+                    {/* <YoutubeLive
+                    iframeWidth={400}
+                    iframeHeight={300}
+                    maxResults={50}
+                    youtubeChannelId='UCqLGw6DbXL8NaiM3p97TA6A '
+                    googleApiKey='{YOUR_GOOGLE_API_KEY}'/> */}
+                    <div style={{paddingLeft:'30%',paddingRight:'25%'}}>
+                    <YouTube
+                    videoId="qAqa_72Nit8"
+                    opts={opts}
+                    />
+                    </div>
+                </Grid>
+            ):  <Grid item xs={12}>
+                <Typography align="center" variant="display3">
+                    Stream Not Available
+                </Typography>
+                </Grid>
+
+        }
         </Grid>
         </div>
         )
